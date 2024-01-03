dd74e93f8180cb7329f5e9bdd6510ff942d0ce9d
Bruno Alex <132787362+alexxsouzaa@users.noreply.github.com>
2024-01-03 18:55:10 -0300

    Update RADAR_ULTRASSONICO.ino

--- RADAR_ULTRASSONICO/RADAR_ULTRASSONICO.ino
+++ RADAR_ULTRASSONICO/RADAR_ULTRASSONICO.ino
@@ -28,7 +28,7 @@
 UltraSonic_HC_SR04 sensor(pinTrigger, pinEcho);
 
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE); // Driver do display OLED DE 1.3 Polegadas
-// U8G2_SSD1306_64X32_1F_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE); // Driver do display OLED DE 0.96 Polegadas
+//U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE, /* clock=*/ SCL, /* data=*/ SDA);  // Driver do display OLED DE 0.96 Polegadas
 
 int distanceCm; // Variável que armazena a distância do sensor em centímetros
 
